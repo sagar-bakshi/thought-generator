51108b92d97a511fc13ba8823823712e3e6533ad
sagar-bakshi <sagarbkshi@gmail.com>
2020-10-01 23:31:39 +0530

    added Quotes api

---
+++ index.js
@@ -0,0 +1,44 @@
+const quote = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('qoute-author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+//get quote from api
+async function getQuote() {
+    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    try{
+        const response = await fetch(proxyurl + apiUrl);
+        const data = await response.json();
+        if (data.quoteAuthor){
+            authorText.innerText = data.quoteAuthor;
+        }else {
+            authorText.innerText = 'unknown'
+        }
+
+        quoteText.innerText = data.quoteText;
+    }catch (e){
+        // //onLoad
+        // getQuote();
+        console.log(e);
+    }
+}
+
+//tweet qoute
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//event listner
+newQuoteBtn.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
+//onLoad
+getQuote();
+
